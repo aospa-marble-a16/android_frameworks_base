e9257e98614597abd3f426aae18571ba60a43c2a
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-08-24 01:04:33 +0530

    Initial implementation of KeyboxImitationHooks
    Co-authored-by: chiteroman <querveros999@gmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: Id0e093cbbd095792c8925cdb48ae631bf86118ad
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Frost <frost.github@proton.me>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/com/android/internal/util/aospa/PixelPropsUtils.java
+++ core/java/com/android/internal/util/aospa/PixelPropsUtils.java
@@ -39,6 +39,7 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import com.android.internal.R;
+import com.android.internal.util.KeyProviderManager;
 import com.android.internal.util.aospa.Utils;
 
 import java.lang.reflect.Field;
@@ -755,6 +756,12 @@ public final class PixelPropsUtils {
         boolean isPixelGmsEnabled = SystemProperties.getBoolean(SPOOF_GMS, true);
         if (!isPixelGmsEnabled) {
             dlog("onEngineGetCertificateChain disabled by setting");
+            return;
+        }
+
+        // If a keybox is found, don't block key attestation
+        if (KeyProviderManager.isKeyboxAvailable()) {
+            dlog("Key attestation blocking is disabled because a keybox is defined to spoof");
             return;
         }
 
